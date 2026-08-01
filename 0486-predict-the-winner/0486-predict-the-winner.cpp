51e5afaca2c2e5f3863a41eb5da3b969eb49514c
Shayon Boidya <153078351+Shayonboidya@users.noreply.github.com>
2026-08-02 03:50:16 +0600

    Time: 0 ms (100%), Space: 9.6 MB (85.04%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    int t[23][23];
     int solved(int i, int j, vector<int> &nums){
         if(i > j){
             return 0;
@@ -7,12 +8,17 @@ public:
         if(i == j){
             return nums[i];
         }
+        if(t[i][j] != -1){
+            return t[i][j];
+        }
+
         int take_i = nums[i] + min(solved(i+2, j,nums), solved(i+1, j-1,nums));
         int take_j = nums[j] + min(solved(i+1, j-1,nums), solved(i, j-2,nums));
 
-        return max(take_i, take_j);
+        return t[i][j] =  max(take_i, take_j);
     }
     bool predictTheWinner(vector<int>& nums) {
+        memset(t,-1,sizeof(t));
         int n = nums.size();
         int total_score = accumulate(nums.begin(), nums.end(), 0);
 
