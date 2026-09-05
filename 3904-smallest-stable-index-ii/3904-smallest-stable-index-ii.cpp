4b923985d434af4572efd866358d2374ae185ee0
Shayon Boidya <153078351+Shayonboidya@users.noreply.github.com>
2026-09-05 21:06:10 +0600

    Time: 12 ms (51.64%), Space: 210.8 MB (56.49%) - LeetHub

--- 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.cpp
+++ 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.cpp
@@ -2,22 +2,29 @@ class Solution {
 public:
     int firstStableIndex(vector<int>& nums, int k) {
         int n = nums.size();
+        vector<int> left(n, 0);
+        vector<int> right(n, 0);
+        int mx = INT_MIN;
+        int mi = INT_MAX;
 
-        vector<int> minFromIndex(n);
-        int minEl = INT_MAX;
-        for(int i = n-1; i >= 0; i--) {
-            minEl = min(minEl, nums[i]);
-            minFromIndex[i] = minEl;
+        for (int i = 0; i < n; i++) {
+            if (mx <= nums[i]) {
+                mx = nums[i];
+            }
+            left[i] = mx;
         }
-
-        int maxEl = 0;
-        for(int i = 0; i < n; i++) {
-            maxEl = max(maxEl, nums[i]);
-
-            if(maxEl - minFromIndex[i] <= k)
+        for (int i = n - 1; i >= 0; i--) {
+            if (mi >= nums[i]) {
+                mi = nums[i];
+            }
+            right[i] = mi;
+        }
+        for (int i = 0; i < n; i++) {
+            int diff = left[i] - right[i];
+            if (diff <= k) {
                 return i;
+            }
         }
-        
         return -1;
     }
 };
